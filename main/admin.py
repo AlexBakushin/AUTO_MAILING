6397b2ab89dc3a9ab6635e17a1c66260e247f35c
AlexBakushin <bakushin35@gmail.com>
2024-01-17 00:53:14 +0500

    add client's CRUD

--- main/admin.py
+++ main/admin.py
@@ -4,27 +4,27 @@ from main.models import Client, Settings, Massage, Logs
 
 @admin.register(Client)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'mail', 'massage')
-    list_filter = ('name',)
-    search_fields = ('name', 'mail', 'massage')
+    list_display = ('first_name', 'last_name', 'mail',)
+    list_filter = ('last_name',)
+    search_fields = ('first_name', 'last_name', 'mail',)
 
 
 @admin.register(Settings)
 class SettingsAdmin(admin.ModelAdmin):
-    list_display = ('time', 'frequency', 'status')
+    list_display = ('time', 'frequency', 'status',)
     list_filter = ('status',)
     search_fields = ('status', 'frequency',)
 
 
 @admin.register(Massage)
 class MassageAdmin(admin.ModelAdmin):
-    list_display = ('head', 'body', 'settings')
+    list_display = ('head', 'body', 'settings',)
     list_filter = ('head',)
     search_fields = ('head',)
 
 
 @admin.register(Logs)
 class LogsAdmin(admin.ModelAdmin):
-    list_display = ('datatime', 'status', 'response')
+    list_display = ('datatime', 'status', 'response',)
     list_filter = ('status',)
     search_fields = ('status', 'response',)
