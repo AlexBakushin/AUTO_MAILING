701f2651ce3f9a9e604b4c58397a8d750ad98ea8
AlexBakushin <bakushin35@gmail.com>
2024-01-21 00:50:35 +0500

    fix buuuuuugss

--- users/models.py
+++ users/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-#from main.models import Client
 
 NULLABLE = {'blank': True, 'null': True}
 
@@ -9,7 +8,6 @@ class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='Почта')
     avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', **NULLABLE)
-    #client = models.ManyToManyField(Client, verbose_name='Клиенты')
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
